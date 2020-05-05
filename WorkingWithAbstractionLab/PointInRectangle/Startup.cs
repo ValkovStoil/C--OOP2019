8fc1828e34fc61df5e8addae3a2c597bed108e54
ValkovStoil <valkovstoil@abv.bg>
2020-05-05 18:07:34 +0300

    Point in Rectangle

---
+++ WorkingWithAbstractionLab/PointInRectangle/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace PointInRectangle
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var rectangleInfo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var topLeftX = rectangleInfo[0];
+            var topLeftY = rectangleInfo[1];
+            var bottomRightX = rectangleInfo[2];
+            var bottomRightY = rectangleInfo[3];
+
+            var topPoint = new Point(topLeftX, topLeftY);
+            var bottomPoint = new Point(bottomRightX, bottomRightY);
+
+            var rectangle = new Rectangle(topPoint, bottomPoint);
+
+            var numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int times = 0; times < numberOfInputs; times++)
+            {
+                var checkPointCoordinates = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var checkPoint = new Point(checkPointCoordinates[0], checkPointCoordinates[1]);
+
+                Console.WriteLine(rectangle.Contains(checkPoint));
+            }
+
+        }
+    }
+}
